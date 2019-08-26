6d21658c4415915c737790475c97517bf1edae3c
Constantine Kormashev <constantine.kormashev@gmail.com>
2019-08-26 09:18:33 +0300

    optimized code

--- spark_ml/notebooks/sentiment_model.py
+++ spark_ml/notebooks/sentiment_model.py
@@ -1,37 +1,29 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import SGDClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
 import re
-import nltk
+
+from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+
 
 def preprocessor(text):
-    cleaned = re.sub("[^a-zA-Z]", " ", text)
-    return cleaned
+    return re.sub("[^a-zA-Z]", " ", text)
+
 
 def tokenizer(text):
-    tokens = [t for t in nltk.word_tokenize(text) if len(t)>3]
-    stems = []
-    for item in tokens:
-        stems.append(PorterStemmer().stem(item))
-    return stems
+    stemmer = PorterStemmer()
+    return [stemmer.stem(t) for t in word_tokenize(text) if len(t) > 3]
 
 
-vectorizer = TfidfVectorizer(preprocessor=preprocessor,tokenizer=tokenizer)
-
+vectorizer = TfidfVectorizer(preprocessor=preprocessor, tokenizer=tokenizer)
 
 pipeline = Pipeline([
     ('tfidf', vectorizer),
     ('clf', RandomForestClassifier()),
 ])
 
-
 parameters = {
     'tfidf__max_df': (0.5, 0.75, 1.0),
-    'clf__n_estimators': (40,60,100),
-    'clf__max_depth': (5,8)
-}
-
+    'clf__n_estimators': (40, 60, 100),
+    'clf__max_depth': (5, 8)}
